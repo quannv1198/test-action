268e4e9a34dfdcc0080c9a11662ffd10cb49a0b2
quannv <quannv1198@gmail.com>
2022-04-07 11:37:16 +0700

    update test

--- home.test.js
+++ home.test.js
@@ -1,3 +1,20 @@
+const puppeteer = require('puppeteer');
+let browser;
+let page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+    page = await browser.newPage();
+	await page.setDefaultNavigationTimeout(10000);
+    await page.goto('https://google.com')
+});
+afterEach(() => {
+    browser.close();
+});
+
 describe('home test', () => {
     it('test', (done) => {
         done()
